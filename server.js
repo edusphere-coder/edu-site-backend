6a9f465e0a02ceb0ce9d7a1eb9e0161aa8ea8076
edusphere-coder <eduspherecourses@gmail.com>
2026-03-31 19:06:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet()); // Security headers
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',"https://edu-site-ftu8-five.vercel.app"
     credentials: true
 }));
 app.use(morgan('dev')); // Logging
